359188b7d4819df803cd0f425ceb1d23931ca097
gaddario98 <gaddario98@gmail.com>
2025-10-17 00:34:07 +0200

    Fix: update package

--- rollup.config.js
+++ rollup.config.js
@@ -4,5 +4,5 @@ import { createReactNativeConfig } from "../../rollup.common.config.js";
 const require = createRequire(import.meta.url);
 const pkg = require("./package.json");
 
-// Utilizziamo direttamente la configurazione semplificata per pacchetti React Native
+// Utilizziamo la configurazione ottimizzata per pacchetti React Native
 export default createReactNativeConfig(pkg);
